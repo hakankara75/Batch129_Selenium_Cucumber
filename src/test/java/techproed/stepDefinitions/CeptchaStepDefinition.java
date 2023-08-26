4c15c1aa0439ce9d03a9def74412c69a229b714c
HakanKara75 <hakanbatirhan@gmail.com>
2023-08-26 18:44:35 +0300

    v

--- src/test/java/techproed/stepDefinitions/CeptchaStepDefinition.java
+++ src/test/java/techproed/stepDefinitions/CeptchaStepDefinition.java
@@ -1,32 +1,105 @@
 package techproed.stepDefinitions;
 
-import io.cucumber.java.en.Given;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import techproed.utilities.ConfigReader;
-import techproed.utilities.Driver;
+
 
 public class CeptchaStepDefinition {
-
-    @Given("captcha")
-    public void captcha() {
-
-        WebDriver driver = new ChromeDriver();
-
-// Set the reCAPTCHA site key and secret key
-        driver.manage().addCookie(new Cookie("reCAPTCHA_SITE_KEY", "your_site_key"));
-        driver.manage().addCookie(new Cookie("reCAPTCHA_SECRET_KEY", "your_secret_key"));
-
-// Go to the website with the reCAPTCHA
-        driver.get("https://www.example.com/");
-
-// Click on the reCAPTCHA checkbox
-        driver.findElement(By.id("reCAPTCHA-checkbox")).click();
-
-// Submit the form
-        driver.findElement(By.id("submit")).click();
-    }
-
+//    static WebDriver driver = new ChromeDriver();
+//    @Given("captcha")
+//    public void captcha() {
+//
+//// Set the reCAPTCHA site key and secret key
+//        driver.manage().addCookie(new Cookie("reCAPTCHA_SITE_KEY", "your_site_key"));
+//        driver.manage().addCookie(new Cookie("reCAPTCHA_SECRET_KEY", "your_secret_key"));
+//
+//// Go to the website with the reCAPTCHA
+//        driver.get("https://www.example.com/");
+//
+//// Click on the reCAPTCHA checkbox
+//        driver.findElement(By.id("reCAPTCHA-checkbox")).click();
+//
+//// Submit the form
+//        driver.findElement(By.id("submit")).click();
+//    }
+//
+//    public class CaptchaSolver {
+//
+//        public static void main(String[] args) throws IOException {
+//            // Create a new WebDriver
+//
+//            // Go to the website with the CAPTCHA
+//            driver.get("https://www.example.com/");
+//
+//            // Take a screenshot of the CAPTCHA
+//            File screenshot = driver.getScreenshotAs(OutputType.FILE);
+//
+//            // Use Tesseract OCR to recognize the text in the CAPTCHA
+//            String captchaText = Tesseract.getOCRText(screenshot);
+//
+//            // Submit the form with the captcha text
+//            driver.findElement(By.id("reCAPTCHA-input")).sendKeys(captchaText);
+//
+//            // Click on the submit button
+//            driver.findElement(By.id("submit")).click();
+//        }
+//    }
+//    public class CAPTCHABot {
+//
+//        private static final String SECRET_KEY = "YOUR_SECRET_KEY";
+//
+//        public static void main(String[] args) {
+//            // Create a new WebDriver
+//
+//            // Go to the website with the CAPTCHA
+//            driver.get("https://www.example.com/");
+//
+//            // Find the CAPTCHA element
+//            WebElement captchaElement = driver.findElement(By.id("reCAPTCHA-image"));
+//
+//            // Take a screenshot of the CAPTCHA
+//            File screenshot = driver.getScreenshotAs(OutputType.FILE);
+//
+//            // Use the CAPTCHABot to solve the CAPTCHA
+//            String captchaText = CAPTCHABot.solve(screenshot);
+//
+//            // Submit the form with the CAPTCHA text
+//            driver.findElement(By.id("reCAPTCHA-input")).sendKeys(captchaText);
+//
+//            // Submit the form
+//            driver.findElement(By.id("submit")).click();
+//        }
+//
+//        public String solve(File screenshot) {
+//            // Create a new CAPTCHABot object
+//            CAPTCHABot bot = new CAPTCHABot();
+//
+//            // Pass the CAPTCHA image to the CAPTCHABot object
+//            bot.setCaptchaImage(screenshot);
+//
+//            // Solve the CAPTCHA
+//            String captchaText = bot.solve();
+//
+//            // Return the CAPTCHA text
+//            return captchaText;
+//        }
+//
+//        private void setCaptchaImage(File screenshot) {
+//            // Set the CAPTCHA image to the CAPTCHABot object
+//            captchaImage = screenshot;
+//        }
+//
+//        private String solve() {
+//            // Solve the CAPTCHA
+//            String captchaText = recognizeText(captchaImage);
+//
+//            // Return the CAPTCHA text
+//            return captchaText;
+//        }
+//
+//        private String recognizeText(File screenshot) {
+//            // Recognize the text in the CAPTCHA image
+//            String captchaText = Tesseract.getOCRText(screenshot);
+//
+//            // Return the CAPTCHA text
+//            return captchaText;
+//        }
 }
