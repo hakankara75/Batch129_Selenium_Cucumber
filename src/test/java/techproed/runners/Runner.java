d0cb801f317a364115901fefdc334c9fab315969
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-04 22:42:24 +0300

    v

--- src/test/java/techproed/runners/Runner.java
+++ src/test/java/techproed/runners/Runner.java
@@ -3,6 +3,7 @@ package techproed.runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+
 /*
 Runner class; testNG deki .xml file larda belirttiğimiz class'ları,packageları veya methodları nasıl
 çalıştırıyorsak, Cucumber frameworkundede Runner class'ındaki tags parametresi ile belirttiğimiz
@@ -12,21 +13,23 @@ senaryoyu çalıştırabiliriz
 @RunWith(Cucumber.class)
 //Seneryoların nerede ve nasıl çalışacağı, hangi raporu kullanacağıyla alakalı seçenekleri ayarlarız
 @CucumberOptions(  //plugin kismi raporlar icin
-                plugin = {
-                        "pretty", //console renkli yazdirmak icin
-                        "html:src/test/resources/features/htmlReport/cucumberHooks.html",
-                        "json:target/json-reports/cucumber.json",
-                        "junit:src/test/resources/features/htmlReport/cucumber.xml",
-                        "rerun:TestOutput/failed_scenario.txt",//rerun hata veren testleri kaydedip yeniden otomatik kosmak icin
-                        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},//spark report icin
-              //  monochrome = true, //console okunakli hale getirir, renksiz yapar
-                features = "src/test/resources/features",
-                 glue = {"techproed/stepDefinitions", "techproed/hooks"},
+        plugin = {
+                "pretty", //console renkli yazdirmak icin
+                "html:src/test/resources/features/htmlReport/cucumberHooks.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:src/test/resources/features/htmlReport/cucumber.xml",
+                "rerun:TestOutput/failed_scenario.txt",//rerun hata veren testleri kaydedip yeniden otomatik kosmak icin
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},//spark report icin
+        //  monochrome = true, //console okunakli hale getirir, renksiz yapar
+        features = "src/test/resources/features",
+        glue = {"techproed/stepDefinitions", "techproed/hooks"},
         /* 1-Bu parametre ile kodlarımızı yazdığımız stepDefinition class'ının packege'ını belirtiriz
         2- Hooks classin yolunu veririz.
                                                      */
-                 tags = "@hooks",
-                 dryRun = false                               )
+        tags = "@hooks",
+        dryRun = false,
+        monochrome = false //false scenario adimlarini renkli gosterir
+)
 /*
 features ===> features'ların olduğu packega'ın yolunu ver(ContentRoot)
 glue ====> stepDefinition'ların olduğu packega'ın yolunu ver(Source Root), Hooks class yolunu ver
